6c4fe0e7594f3587d2547db754aee2aec74834f8
NibbleByte <NibbleByte3@gmail.com>
2026-07-29 21:38:51 +0300

    Moved the input framework to WiseInput folder. Will decouple it from the GFrame next.

---
+++ UIInputDisplay/HotkeyDisplayUIScopeElement.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DevLocker.GFrame.Input.UIInputDisplay
+{
+	/// <summary>
+	/// Displays hotkey icon / text.
+	/// Refreshes if devices change.
+	/// Will be controlled by UIScope.
+	/// </summary>
+	public class HotkeyDisplayUIScopeElement : HotkeyDisplayUI, UIScope.IScopeElement
+	{
+
+	}
+
+}
---
+++ UIScopes/ActivateOnUIScopeFocusedScopeElement.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DevLocker.GFrame.Input.UIScope
+{
+	/// <summary>
+	/// Monitors the <see cref="UIScope"/> and shows or hides objects if focused.
+	/// </summary>
+	public class ActivateOnUIScopeFocusedScopeElement : ActivateOnUIScopeFocused, IScopeElement
+	{
+
+	}
+}
